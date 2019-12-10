125ce3948ba5b48443fcd0fdcd8d4b121c961c2f
Jigyasu Prakash <jigyasuprakash3@gmail.com>
2019-12-10 23:45:35 +0530

    basic properties achieved

---
+++ README.md
@@ -0,0 +1,3 @@
+# Basic Todo App made with React
+
+- This is a basic todo list app made with using React.
---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import Header from './components/layout/Header';
+import uuid from 'uuid';
+import './App.css';
+
+export class App extends Component {
+  state = {
+    todos: [
+      {
+        id: uuid.v4(),
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: uuid.v4(),
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: uuid.v4(),
+        title: 'Meeting with boss',
+        completed: false
+      },
+    ]
+  }
+
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    });
+  }
+
+  delTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  }
+
+  addTodo = (title) => {
+    const newTodo= {
+      id: uuid.v4(),
+      title,
+      completed: false
+    }
+    this.setState({
+      todos:[...this.state.todos, newTodo]
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo} />
+          <Todos todos={this.state.todos}
+            markComplete={this.markComplete}
+            delTodo={this.delTodo} />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
