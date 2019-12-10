a62d92d7c9889641616bab75213460c751595481
Jigyasu Prakash <jigyasuprakash3@gmail.com>
2019-12-11 00:12:40 +0530

    React-Route functionality used

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import Header from './components/layout/Header';
+import About from './components/pages/About';
 import uuid from 'uuid';
 import './App.css';
 
@@ -44,27 +46,34 @@ export class App extends Component {
   }
 
   addTodo = (title) => {
-    const newTodo= {
+    const newTodo = {
       id: uuid.v4(),
       title,
       completed: false
     }
     this.setState({
-      todos:[...this.state.todos, newTodo]
+      todos: [...this.state.todos, newTodo]
     });
   }
 
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo addTodo={this.addTodo} />
-          <Todos todos={this.state.todos}
-            markComplete={this.markComplete}
-            delTodo={this.delTodo} />
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo} />
+                <Todos todos={this.state.todos}
+                  markComplete={this.markComplete}
+                  delTodo={this.delTodo} />
+              </React.Fragment>
+            )} />
+            <Route path="/about" component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     )
   }
 }
